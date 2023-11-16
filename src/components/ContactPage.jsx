f73633d194f272d5757c017012773b972b2b0dce
Stan Strawa <stan.strawa@guavestudios.com>
2023-11-16 14:51:24 +0100

    hide email

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,12 +1,22 @@
-import { h } from 'preact';
-
 function Contact() {
+    const user = 'hi';
+    const domain = 'stanstrawa';
+    const tld = 'dev';
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        window.location.href = `mailto:${user}@${domain}.${tld}`;
+    };
+
     return (
         <div className="text container" data-id="contact">
             <div className="text__content text__content--contact">
-                <a href="mailto:hi@stanstrawa.dev" className="contact-link"> hi@stanstrawa.dev</a>
-                <a href="https://www.linkedin.com/in/stan-strawa/" className="contact-link" target="_blank"> linkedin.com</a>
-                <a href="https://github.com/Stanjo84" className="contact-link" target="_blank"> github.com</a>
+                <a href="#" onClick={handleClick} className={"contact-link"} aria-label="Send an email to hi at stanstrawa dot dev" rel={"nofollow"}>
+                    hi[at]stanstrawa[dot]dev
+                </a>
+                <a href="https://www.linkedin.com/in/stan-strawa/" className={"contact-link"} target="_blank" rel={"noreferrer"}> linkedin.com</a>
+                <a href="https://github.com/Stanjo84" className={"contact-link"} target="_blank" rel={"noreferrer"}> github.com</a>
+                <a href="https://codepen.io/stanjo_1421/" className={"contact-link"} target="_blank" rel={"noreferrer"}> codepen.io</a>
             </div>
         </div>
     );
